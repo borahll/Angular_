acb124114198c8019831122d6e06c2e69ec34ebc
borahll <bora.haliloglu@ug.bilkent.edu.tr>
2023-07-20 08:52:47 +0300

    checkout component

--- shopApp/src/app/shop/checkout/checkout.component.ts
+++ shopApp/src/app/shop/checkout/checkout.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Order } from 'src/app/model/order.model';
+import { OrderRepository } from 'src/app/model/order.repository';
 
 @Component({
   selector: 'checkout',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent {
-
+  orderSent: boolean = false;
+  submitted: boolean = false;
+  constructor(public order: Order, private oderRepo: OrderRepository) {
+    
+  }
+  submitOrder(form: NgForm){
+    this.submitted = true;
+    if(form.valid){
+      this.oderRepo.saveOrder(this.order)
+      .subscribe(order=>{
+        this.order.clear();
+        this.orderSent = true;
+        this.submitted = false;
+      });
+    }
+  }
 }
