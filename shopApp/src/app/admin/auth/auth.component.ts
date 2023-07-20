36d5108d4621f47a452b6b7789589a1d11548b87
borahll <bora.haliloglu@ug.bilkent.edu.tr>
2023-07-20 11:21:38 +0300

    added admin form

--- shopApp/src/app/admin/auth/auth.component.ts
+++ shopApp/src/app/admin/auth/auth.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent {
+  public username: string = "";
+  public password: string = "";
+  public errorMessage:string = "";
+  constructor(private router: Router){
 
+  }
+  login(form: NgForm){
+    if(form.valid){
+      if(this.username == 'admin' && this.password == "loooo"){
+        this.router.navigateByUrl('/admin/main');
+      }
+      else{
+        this.errorMessage = "unauthorized";
+      }
+    }
+    else{
+      this.errorMessage = "invalid form";
+    }
+  }
 }
