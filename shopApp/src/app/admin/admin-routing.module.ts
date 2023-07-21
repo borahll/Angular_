2f30dc4546c1f05f363067151ea5a7bf7cf4232d
borahll <bora.haliloglu@ug.bilkent.edu.tr>
2023-07-21 12:21:40 +0300

    bug fix

--- shopApp/src/app/admin/admin-routing.module.ts
+++ shopApp/src/app/admin/admin-routing.module.ts
@@ -3,11 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { AdminComponent } from './admin.component';
 import { AuthGuard } from './auth.guard';
-import { ProductFormComponent } from './products/product-form/product-form.component';
-import { ProductListComponent } from '../shop/product-list/product-list.component';
 import { CategoryFormComponent } from './categories/category-form/category-form.component';
 import { OrderListComponent } from './order/order-list/order-list.component';
 import { CategoryListComponent } from './categories/category-list/category-list.component';
+import { ProductFormComponent } from './products/product-form/product-form.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
 
 const routes: Routes = [
   {path: 'auth', component: AuthComponent},
@@ -17,7 +17,7 @@ const routes: Routes = [
     children:[
     { path: 'products/:mode/:id', component: ProductFormComponent },
     { path: 'products/:mode/', component: ProductFormComponent },
-    { path: 'products', component: ProductListComponent },
+    { path: 'products', component:  ProductListComponent},
     { path: 'categories/:mode/:id', component: CategoryFormComponent },
     { path: 'categories/:mode/', component: CategoryFormComponent },
     { path: 'categories', component: CategoryListComponent },
